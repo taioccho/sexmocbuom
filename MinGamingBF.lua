595ffc3949f4b65e84fcd5e5521742ba5395d2e2
taioccho <letaixinhgai@gmail.com>
2026-07-02 09:04:24 +0700

    Add files via upload

--- MinGamingBF.lua
+++ MinGamingBF.lua
@@ -311,7 +311,7 @@ v5:Destroy();
 --         pcall(v422);
 --     end
 -- end);
-local v14 = loadstring(game:HttpGet("https://raw.githubusercontent.com/obfalchx/testfluent/refs/heads/main/FluentTrau"))();
+local v14 = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local v15 = v14:CreateWindow({
     Title = "Min Gaming",
     SubTitle = "",
